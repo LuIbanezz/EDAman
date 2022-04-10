9ba2b5784b62d71a02dce92162cab69b0fc0d2d9
Santiago Michelotti <santimichelotti@live.com>
2022-04-10 17:46:57 -0300

    .

--- Player.cpp
+++ Player.cpp
@@ -17,8 +17,9 @@ void Player::start()
 	// TODO: Ver si lo que le mandamos al display va en start o en el constructor
 	setDisplay(0);
 	setEyes(YELLOW, YELLOW);
-	//liftTo(0, -9);
-    robotLocation = { 0.0,1.0,ROTATION_DOWN };
+	liftTo(0, -0.8);
+	robotLocation = getRobotSetpoint({ 14,26 }, ROTATION_DOWN);
+	setSetpoint(robotLocation);
 
 }
 
@@ -27,26 +28,13 @@ void Player::update(float deltaTime)
 	//vel que queremos m/s (fija(dato)) * 100 cm/m * deltaTime 
 	//0.64 * 100 * delta time ===>sumamos en la coordenada X o Y
 	//0.72 """"""
-    
-    if(isNextLocationPosible(deltaTime))
-    {
-		if (lastKeyPressed == KEY_UP)
-		{
-            setSetpoint(robotLocation);
-		}
-		else if (lastKeyPressed == KEY_RIGHT)
-		{
-            setSetpoint(robotLocation);
-		}
-		else if (lastKeyPressed == KEY_LEFT)
-		{
-            setSetpoint(robotLocation);
-		}
-		else if (lastKeyPressed == KEY_DOWN)
-		{
-            setSetpoint(robotLocation);
-		}
-    }
+
+
+	if (isNextLocationPosible(deltaTime))
+	{
+		setSetpoint(robotLocation);
+
+	}
 }
 
 void Player::setKeyboardKey(KeyboardKey lastKeyPressed)
@@ -56,34 +44,36 @@ void Player::setKeyboardKey(KeyboardKey lastKeyPressed)
 
 bool Player::isNextLocationPosible(float deltaTime)
 {
-    RobotSetpoint robotFutureLocation = robotLocation;
+	RobotSetpoint robotFutureLocation = robotLocation;
+	//robotFutureLocation.positionZ += 0.009;
+	//robotFutureLocation.positionX += 0.009;
 
-    if (lastKeyPressed == KEY_UP)
-    {
-        robotFutureLocation.positionZ += 0.064 * deltaTime;
-        robotFutureLocation.rotation = ROTATION_UP;
-    }
-    else if (lastKeyPressed == KEY_RIGHT)
-    {
-        robotFutureLocation.positionX += 0.064 * deltaTime;
-        robotFutureLocation.rotation = ROTATION_RIGHT;
-    }
-    else if (lastKeyPressed == KEY_LEFT)
-    {
-        robotFutureLocation.positionX -= 0.064 * deltaTime;
-        robotFutureLocation.rotation = ROTATION_LEFT;
-    }
-    else if (lastKeyPressed == KEY_DOWN)
-    {
-        robotFutureLocation.positionZ -= 0.064 * deltaTime;
-        robotFutureLocation.rotation = ROTATION_DOWN;
-    }
-    
-    if (gameModel->isTileFree(getMazePosition(robotFutureLocation)))
-    {
-        robotLocation = robotFutureLocation;
-        return true;
-    }
+	if (lastKeyPressed == KEY_UP)
+	{
+		robotFutureLocation.positionZ += 1.088 * deltaTime;
+		robotFutureLocation.rotation = ROTATION_UP;
+	}
+	else if (lastKeyPressed == KEY_RIGHT)
+	{
+		robotFutureLocation.positionX += 1.088 * deltaTime;
+		robotFutureLocation.rotation = ROTATION_RIGHT;
+	}
+	else if (lastKeyPressed == KEY_LEFT)
+	{
+		robotFutureLocation.positionX -= 1.088 * deltaTime;
+		robotFutureLocation.rotation = ROTATION_LEFT;
+	}
+	else if (lastKeyPressed == KEY_DOWN)
+	{
+		robotFutureLocation.positionZ -= 1.088 * deltaTime;
+		robotFutureLocation.rotation = ROTATION_DOWN;
+	}
 
-    return false;
+	if (gameModel->isTileFree(getMazePosition(robotFutureLocation)))
+	{
+		robotLocation = robotFutureLocation;
+		return true;
+	}
+
+	return false;
 }
